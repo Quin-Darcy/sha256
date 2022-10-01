cb5ad26620bb51b4ac232b9600193bc0b6d51dc8
Nimrafets Lamensta <qdarcy@csus.edu>
2022-09-30 21:00:13 -0700

    reverted changes

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 mod utils;
 
-pub fn get_hash(flag: u8, data: &str) -> Vec<u32> {
+pub fn get_hash(flag: u8, data: &str) -> String {
     let mut message: Vec<u32> = Vec::new();
     if flag == 102_u8 {
         message = match utils::msg::get_binary(data) {

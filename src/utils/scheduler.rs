10cbaecf32a207e3bfe19212436d0d609b21652b
Quin-Darcy <quintondarcy@csus.edu>
2023-12-31 16:06:54 -0800

    Added wrapped adding to handle overflow

--- src/utils/scheduler.rs
+++ src/utils/scheduler.rs
@@ -56,14 +56,16 @@ pub fn digest(parsed_message: Vec<Vec<u32>>) -> Vec<u32> {
         
         for t in 0..WORDS_IN_SCHEDULE {
             let k: u32 = sha::sha_constants(FIRST_PRIMES[t], 3);
-            
+
             let t1: u32 = h
                 .wrapping_add(sha::SIGMA_1_256(&e))
                 .wrapping_add(sha::ch(&e, &f, &g))
                 .wrapping_add(k)
                 .wrapping_add(message_schedule[t]);
 
-            let t2: u32 = sha::SIGMA_0_256(&a)+sha::maj(&a, &b, &c);
+            let t2: u32 = sha::SIGMA_0_256(&a)
+                .wrapping_add(sha::maj(&a, &b, &c));
+
             h = g;
             g = f;
             f = e;
